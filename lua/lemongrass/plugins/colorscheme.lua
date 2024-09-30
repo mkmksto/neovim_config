e9b6ff05983138cc8c62498c04057878800d6e3b
mkmksto <119323664+mkmksto@users.noreply.github.com>
2024-09-30 17:07:42 +0800

    tokyodark

--- lua/lemongrass/plugins/colorscheme.lua
+++ lua/lemongrass/plugins/colorscheme.lua
@@ -1,9 +1,22 @@
 return {
     -- "Shatur/neovim-ayu",
     "catppuccin/nvim",
+    -- "ghifarit53/tokyonight-vim",
     priority = 1000,
     config = function()
         -- vim.cmd.colorscheme("ayu-mirage")
         vim.cmd.colorscheme("catppuccin-macchiato")
+        -- vim.cmd.colorscheme("tokyonight")
     end,
 }
+
+-- return {
+--     "tiagovla/tokyodark.nvim",
+--     opts = {
+--         -- custom options here
+--     },
+--     config = function(_, opts)
+--         require("tokyodark").setup(opts) -- calling setup is optional
+--         vim.cmd([[colorscheme tokyodark]])
+--     end,
+-- }
