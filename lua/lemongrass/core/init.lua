75b227c6155ea796f47e243f1a9b501a0d4e25fb
mkmksto <119323664+mkmksto@users.noreply.github.com>
2025-01-26 12:30:53 +0800

    feat: disable codeium, enable clipboard on mac

--- lua/lemongrass/core/init.lua
+++ lua/lemongrass/core/init.lua
@@ -3,15 +3,15 @@ require("lemongrass.core.options")
 
 local current_os = vim.loop.os_uname().sysname
 local is_win = string.find(current_os, "Windows")
-
 local is_linux = string.find(current_os, "Linux")
+local is_mac = string.find(current_os, "Darwin")
 -- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
 local is_wsl = (function()
     local output = vim.fn.systemlist("uname -r")
     return not not string.find(output[1] or "", "WSL")
 end)()
 
-if is_linux then
+if is_linux or is_mac then
     require("lemongrass.core.linux")
 end
 
--- lua/lemongrass/plugins/codeium.lua
+++ lua/lemongrass/plugins/codeium.lua
@@ -2,6 +2,7 @@ return {
     -- https://github.com/Exafunction/codeium.vim
     "Exafunction/codeium.vim",
     event = "BufEnter",
+    enabled = false,
     config = function()
         vim.keymap.set("i", "<C-Enter>", function()
             return vim.fn["codeium#Accept"]()
